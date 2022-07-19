f2d0a3e0c2433cd978142e90e4981e672621a664
johnbane85 <junyu.siew@gmail.com>
2022-07-19 12:03:47 +0800

    12:03pm commit

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartService.java
@@ -0,0 +1,35 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartService {
+
+  public List<Item> getShoppingItems() {
+
+    List<Item> lstItems = new ArrayList<>();
+
+    Item itm = new Item();
+    itm.setItemName("Prada Bag");
+    itm.setQuantity(5);
+    lstItems.add(itm);
+
+    itm = new Item();
+    itm.setItemName("Chanel Wallet");
+    itm.setQuantity(2);
+    lstItems.add(itm);
+
+    itm = new Item();
+    itm.setItemName("MacBook Pro");
+    itm.setQuantity(10);
+    lstItems.add(itm);
+
+    itm = new Item();
+    itm.setItemName("LV Tot Bag");
+    itm.setQuantity(8);
+    lstItems.add(itm);
+
+    return lstItems;
+  }
+
+}
---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/Item.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss.day12wkshp;
+
+public class Item {
+  private String itemName;
+  private Integer quantity;
+
+  public String getItemName() {
+    return this.itemName;
+  }
+
+  public void setItemName(String n) {
+    this.itemName = n;
+  }
+
+  public Integer getQuantity() {
+    return this.quantity;
+  }
+
+  public void setQuantity(Integer i) {
+    this.quantity = i;
+  }
+}
