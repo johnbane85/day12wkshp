11c9a852d0112720053c8bcf314713ee4d13a081
johnbane85 <junyu.siew@gmail.com>
2022-07-19 11:31:18 +0800

    update html file

--- src/main/java/sg/edu/nus/iss/day12wkshp/IndexResource.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/IndexResource.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss.day12wkshp;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,11 @@ public class IndexResource {
 
   @GetMapping(produces = { "text/html" })
   public String index(Model model) {
+    model.addAttribute("currTime", (new Date()).toString());
+
+    Calendar cal = Calendar.getInstance();
+    model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
+
     return "indexresource";
   }
 
